ce3261c2572428f3f38912c9c93761f5dec3c2c3
Pype <renaneverton.lopes@gmail.com>
2020-01-23 19:10:50 -0300

    skel

---
+++ src/maze.c
@@ -0,0 +1,23 @@
+/*
+ *  Maze Thread 
+ *  |   -   --thread 5 (RANGE = RAND(T*10))
+ *
+*/
+// MAZE HEADER
+#include "maze.h"
+#include <stdio.h>
+#include <getopt.h>
+
+int
+orgarg(int argc, const char* argv[])
+{
+
+}
+
+int 
+main(int argc, const char* argv[])
+{
+    
+
+    return 0;
+}
---
+++ src/maze.h
@@ -0,0 +1,12 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+#define MAZE_VERSION 0.1
+#define AUTHOR "pype"
+
+struct racebj {
+    int runners;
+    int endline;
+};
+
+#endif
