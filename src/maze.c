de6bec7b9981b623f99ed5d84a930440eaf413e1
Renan Lopes <renaneverton.lopes@gmail.com>
2020-01-24 05:21:36 -0300

    maze.*

--- src/maze.c
+++ src/maze.c
@@ -1,24 +1,66 @@
 /*
  *  Maze Thread 
- *  |   -   --thread 5 (RANGE = RAND(T*10))
- *
+ *  |   -   --thread|-t 5 (RANGE = RAND(T*10)) 
+ *  |   -   --help|-h
 */
 // MAZE HEADER
 #include "maze.h"
 #include <stdio.h>
 #include <getopt.h>
+#include <stdlib.h>
+
+#define die(STR)    \
+{                   \
+    printf(STR);    \
+    exit(1);        \
+}
+
+
+void help()
+{
+    printf("Welcome to Maze Thread Race 2020\n"
+           "-t\t- number of thread.. or runners\n"
+           "-h\t- this message output\n");
+    die("life is short, try u best or die like the rest\n");
+}
 
 int
-orgarg(int argc, const char* argv[])
+orgarg(int argc,  char** argv, struct racebj* raceopt)
 {
+    int copt,ret = 0;
 
-    return 0;
+    while((copt = getopt(argc, argv, "ht:")) != -1)
+    {
+        switch(copt)
+        {
+            case 'h':
+                ret = 1;
+                break;
+            case 't':
+                raceopt->runners = strtol(optarg, NULL, 10) > MAX_THREAD ? MAX_THREAD : strtol(optarg, NULL, 10); 
+                break;
+            case '?':
+                ret = 1; 
+                break;
+             default:
+                ret = 1;
+        }    
+    }
+
+    return ret;
 }
 
 int 
-main(int argc, const char* argv[])
+main(int argc, char* argv[])
 {
+    struct racebj race;
     
+    
+    if(orgarg(argc, argv, &race) != 0 || argc < 2) // Organize the arguments 
+        help();
+
+    
+
 
     return 0;
 }
--- src/maze.h
+++ src/maze.h
@@ -3,10 +3,10 @@
 #define MAZE_VERSION 0.1
 #define AUTHOR "pype"
 
-#define MIN_THREAD 2
-#define MAX_THREAD 255 // /proc/sys/kernel/threads-max 
+#define MIN_THREAD 4 // the more competitors the better
+#define MAX_THREAD 255 // ignore > /proc/sys/kernel/threads-max 
 
 struct racebj {
-    int runners;
-    int endline;
+    unsigned int runners;
+    unsigned int endline;
 };
